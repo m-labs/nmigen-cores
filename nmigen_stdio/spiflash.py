d665944b2334ba68531a1527cca772a034f28930
Harry Ho <hh@m-labs.hk>
2019-11-22 17:43:18 +0800

    spiflash: implement FAST READ protocol, add simple simulation - currently only supporting Dual and Quad protocols

---
+++ nmigen_stdio/spiflash.py
@@ -0,0 +1,281 @@
+from nmigen import *
+from nmigen.lib.io import Pin
+from nmigen.build.dsl import Subsignal, Pins
+from nmigen.build.plat import Platform
+from nmigen.utils import bits_for, log2_int
+
+
+__all__ = ["SPIFlash"]
+
+
+
+class SPIFlash(Elaboratable):
+
+    def _check_commands(self, cmd_dict):
+        cmd_names = [
+            "FAST_READ"
+            # TODO: Add more supported commands
+        ]
+        for cmd in cmd_dict:
+            if cmd not in cmd_names:
+                raise ValueError("Invalid SPI command {!r}; must be one of {}"
+                                 .format(cmd, cmd_names))
+            if cmd_dict[cmd] > 2**self.cmd_width:
+                raise ValueError("Invalid SPI command value {!r}; must not be more than {}-bit wide"
+                                 .format(cmd_dict[cmd], 2**self.cmd_width))
+
+
+    def _format_cmd(self, cmd_value):
+        """
+        Returns the values on all the DQ lines that corresponds to the command.
+
+        Since everything is transmitted on all DQ lines (command, address and data),
+        the input cmd_value is extended/interleaved to full DQ width
+        even if DQ1-DQ3 are "don't care" during the command phase:
+        
+        eg:    1    1    1    0    1    0    1    1   (extended SPI mode)
+              11   11   11   10   11   10   11   11   (dual I/O SPI mode)
+            1111 1111 1111 1110 1111 1110 1111 1111   (quad I/O SPI mode)
+        """
+        fcmd = 2**(self.cmd_width*self.spi_width) - 1
+        for bit in range(self.cmd_width):
+            if (cmd_value >> bit)%2 == 0:
+                fcmd &= ~(1 << (bit*self.spi_width))
+        return fcmd
+
+
+    def __init__(self, *, protocol, addr_width, data_width, cmd_width, cmd_dict, 
+                 dummy_cycles=15, divisor=1, device=None, pins=None):
+        if protocol not in ["extended", "dual", "quad"]:
+            raise ValueError("Invalid SPI protocol {!r}; must be one of \"extended\", \"dual\", or \"quad\""
+                             .format(protocol))
+        self._protocol = protocol
+        self.addr_width = addr_width
+        self.data_width = data_width
+        self.cmd_width = cmd_width
+        if not isinstance(cmd_dict, dict):
+            raise TypeError("Invalid command dict {!r}; must be a dict where each key-value pair is (command name, command value)"
+                            .format(cmd_dict))
+        self._check_commands(cmd_dict)
+        self._cmd_dict = cmd_dict
+        self.dummy_cycles = dummy_cycles
+        if divisor < 1:
+            raise ValueError("Invalid divisor value; must be at least 1"
+                             .format(divisor))
+        self.divisor = Signal(bits_for(divisor), reset=divisor)
+        self._divisor_val = divisor + 1
+
+        supported_devices = ["lattice_ecp5"]
+        if device is not None and device not in supported_devices:
+            raise ValueError("Invalid FPGA device name {!r}; must be one of {}"
+                             .format(device, supported_devices))
+        self._device = device
+        if self._device is not None and pins is None:
+            raise ValueError("Pins parameter is missing for this FPGA device {}"
+                             .format(self._device))
+        self._pins = pins
+
+        if self._protocol == "extended":
+            self.spi_width = 1
+        elif self._protocol == "dual":
+            self.spi_width = 2
+        elif self._protocol == "quad":
+            self.spi_width = 4
+
+        self.cs = Signal(reset=0)       # Equivalent to ~CS_N
+        self.clk = Signal()
+
+        if protocol == "extended":
+            self.mosi = Signal()
+            self.miso = Signal()
+            # TODO: Add wp & hold pins
+        elif protocol == "dual":
+            self.dq = Pin(2, "io")
+        elif protocol == "quad":
+            self.dq = Pin(4, "io")
+
+        self.rdy = Signal()             # Internal rdy signal, set at idle state
+        self.ack = Signal()             # External ack signal that begins a read
+        self.cmd = Signal(cmd_width)    # set to the corresponding value of the command
+        self.addr = Signal(addr_width)
+        self.r_data = Signal(self.data_width)
+        self.r_rdy = Signal()           # 1 if r_data is valid, 0 otherwise
+
+        self.fcmd_width = self.cmd_width * self.spi_width
+        # A two-way register storing the current value on the DQ I/O pins
+        # (Note: DQ pins are both input/output only for Dual or Quad SPI protocols)
+        self.shreg = Signal(max(self.fcmd_width, self.addr_width, self.data_width))
+        self.counter = Signal.like(self.divisor)
+
+
+    def _add_clk_primitive(self, module):
+        """Add a submodule whose instantiation is required by certain devices
+        when choosing a user clock as SPI clock
+        """
+        # Lattice ECP5:
+        # "The ECP5 and ECP5-5G devices provide a solution for users 
+        # to choose any user clock as MCLK under this scenario 
+        # by instantiating USRMCLK macro in your Verilog or VHDL."
+        # (see Section 6.1.2 of FPGA-TN-02039-1.7, 
+        #  "ECP5 and ECP5-5G sysCONFIG Usage Guide Technical Note")
+        if self._device == "lattice_ecp5":
+            module.submodules += Instance("USRMCLK",
+                                          i_USRMCLKI=self._pins.clk,
+                                          i_USRMCLKTS=self._pins.cs.o)
+
+
+    def elaborate(self, platform):
+        m = Module()
+
+        shreg = self.shreg
+        counter = self.counter
+
+        if self._pins is not None:
+            self._add_clk_primitive(m)
+            m.d.comb += [
+                self._pins.cs.o.eq(self.cs),
+                self._pins.clk.eq(self.clk)
+            ]
+            if self._protocol == "extended":
+                m.d.comb += [
+                    MultiReg(self._pins.miso.i, self.miso),
+                    self._pins.mosi.o.eq(self.mosi)
+                ]
+            elif self._protocol in ["dual", "quad"]:
+                dq_oe = Signal()
+                m.submodules.dq = platform.get_tristate(self.dq, self._pins.dq, None, False)
+        # If the user doesn't give pins, create dq Pins
+        else:
+            dq_oe = Signal()
+            if self._protocol == "dual":
+                dq_pins = Record([
+                    ("dq", Pin(width=2, dir="io", xdr=0).layout)
+                ])
+            elif self._protocol == "quad":
+                dq_pins = Record([
+                    ("dq", Pin(width=4, dir="io", xdr=0).layout)
+                ])
+            tristate_submodule = Module()
+            tristate_submodule.submodules += Instance("$tribuf",
+                p_WIDTH=self.dq.width,
+                i_EN=self.dq.oe,
+                i_A=Platform._invert_if(False, self.dq.o),
+                o_Y=dq_pins
+            )
+            m.submodules.dq = tristate_submodule
+
+        # r_data always reads from shreg
+        m.d.comb += self.r_data.eq(shreg)
+
+        # Countdown logic for counter based on divisor
+        # Also implements MISO logic
+        dq_i = Signal(self.spi_width)
+        ## When countdown is half-way done, clock edge goes up (positive);
+        ##     MISO starts latching bit/byte from slave
+        with m.If(counter == self._divisor_val >> 1):
+            m.d.sync += self.clk.eq(1)
+            if self._protocol == "extended":
+                pass    # TODO
+            elif self._protocol in ["dual", "quad"]:
+                m.d.sync += dq_i.eq(self.dq.i)
+        ## When countdown reaches 0, clock edge goes down (negative)
+        ##     shreg latches from MISO for r_data to read
+        with m.If(counter == 0):
+            m.d.sync += [
+                self.clk.eq(0),
+                self.counter.eq(self.divisor)
+            ]
+            if self._protocol == "extended":
+                pass    # TODO
+            elif self._protocol in ["dual", "quad"]:
+                m.d.sync += shreg.eq(Cat(dq_i, shreg[:-self.spi_width])) # "pushing" old data out from the left
+        ## Normal countdown
+        with m.Else():
+            m.d.sync += counter.eq(counter - 1)
+
+        # MOSI logic for Dual and Quad SPI protocols:
+        # Whenever DQ output should be enabled, 
+        # DQ always output the leftmost `spi_width`-wide bits of shreg
+        if self._protocol in ["dual", "quad"]:
+            m.d.comb += [
+                self.dq.o.eq(shreg[-self.spi_width:]),
+                self.dq.oe.eq(dq_oe)
+            ]
+
+        # Command: FAST READ
+        if "FAST_READ" in self._cmd_dict:
+            fcmd = self._format_cmd(self._cmd_dict["FAST_READ"])
+            if self._protocol == "extended":
+                # TODO: implement FAST READ for Extended SPI protocol as well
+                pass
+
+            elif self._protocol in ["dual", "quad"]:
+                # addr: convert bus address to byte-sized address
+                byte_addr = Cat(Repl(0, log2_int(self.data_width//8)), self.addr)
+                # FSM
+                with m.FSM() as fsm:
+                    state_durations = {
+                        "FASTREAD-CMD"     : self._divisor_val*(self.cmd_width//self.spi_width),
+                        "FASTREAD-ADDR"    : self._divisor_val*(self.addr_width//self.spi_width),
+                        "FASTREAD-WAITREAD": self._divisor_val*(self.dummy_cycles+
+                                                                self.data_width//self.spi_width),
+                        "FASTREAD-RDYWAIT" : 1+self._divisor_val
+                    }
+                    max_duration = max([dur for state,dur in state_durations.items()])
+                    # A "count-up" counter for each state of the command
+                    state_counter = Signal(range(max_duration))
+                    # State: Idling
+                    with m.State("FASTREAD-IDLE"):
+                        m.d.comb += self.rdy.eq(1)
+                        with m.If((self.cmd == self._cmd_dict["FAST_READ"]) &
+                                  self.ack & (counter == 0)):
+                            m.d.sync += [
+                                state_counter.eq(0),
+                                dq_oe.eq(1),
+                                self.cs.eq(1),
+                                shreg[-self.cmd_width:].eq(fcmd)
+                            ]
+                            m.next = "FASTREAD-CMD"
+                    # State: Command, MOSI
+                    with m.State("FASTREAD-CMD"):
+                        with m.If(state_counter == state_durations["FASTREAD-CMD"] - 1):
+                            m.d.sync += [
+                                state_counter.eq(0),
+                                shreg[-self.addr_width:].eq(byte_addr)
+                            ]
+                            m.next = "FASTREAD-ADDR"
+                        with m.Else():
+                            m.d.sync += state_counter.eq(state_counter + 1)
+                    # State: Address, MOSI
+                    with m.State("FASTREAD-ADDR"):
+                        with m.If(state_counter == state_durations["FASTREAD-ADDR"] - 1):
+                            m.d.sync += [
+                                state_counter.eq(0),
+                                dq_oe.eq(0)
+                            ]
+                            m.next = "FASTREAD-WAITREAD"
+                        with m.Else():
+                            m.d.sync += state_counter.eq(state_counter + 1)
+                    # State: Dummy cycles (waiting), and then Read (MISO)
+                    with m.State("FASTREAD-WAITREAD"):
+                        with m.If(state_counter == state_durations["FASTREAD-WAITREAD"] - 1):
+                            m.d.sync += [
+                                state_counter.eq(0),
+                                self.cs.eq(0),
+                                self.r_rdy.eq(1)
+                            ]
+                            m.next = "FASTREAD-RDYWAIT"
+                        with m.Else():
+                            m.d.sync += state_counter.eq(state_counter + 1)
+                    # State: Send r_rdy (for 1 clock period), and then Wait
+                    with m.State("FASTREAD-RDYWAIT"):
+                        with m.If(state_counter == 0):
+                            m.d.sync += self.r_rdy.eq(0)
+                        # Early check to skip 1 clock period of doing nothing
+                        with m.If(state_counter == state_durations["FASTREAD-RDYWAIT"] - 2):
+                            m.d.sync += state_counter.eq(0)
+                            m.next = "FASTREAD-IDLE"
+                        with m.Else():
+                            m.d.sync += state_counter.eq(state_counter + 1)
+
+        return m
---
+++ nmigen_stdio/test/test_spiflash.py
@@ -0,0 +1,117 @@
+import unittest
+from nmigen import *
+from nmigen.lib.fifo import SyncFIFO
+from nmigen.back.pysim import *
+
+from ..spiflash import *
+
+
+def simulation_test(dut, process):
+    with Simulator(dut, vcd_file=open("test.vcd", "w")) as sim:
+        sim.add_clock(1e-6)
+        sim.add_sync_process(process)
+        sim.run()
+
+
+class SPIFlashFastReadTestCase(unittest.TestCase):
+    def divisor_period(self):
+        for _ in range((yield self.dut.divisor) + 1):
+            yield
+
+    class SuperNaiveDualFlash(Elaboratable):
+        def __init__(self, *, dut, data, data_size):
+            self.dut = dut
+            self.data = data
+            self.data_size = data_size
+        def elaborate(self, platform):
+            m = Module()
+            data_sig = Signal(self.data_size)
+            recv_data = SyncFIFO(width=self.dut.spi_width,
+                                 depth=(self.dut.cmd_width+self.dut.addr_width)//self.dut.spi_width)
+            stored_data = SyncFIFO(width=self.dut.spi_width,
+                                   depth=self.dut.data_width//self.dut.spi_width)
+            stored_data_num_sent = Signal(range(stored_data.depth+1),
+                                          reset=stored_data.depth)
+            dummy_counter = Signal(range(self.dut.dummy_cycles),
+                                   reset=self.dut.dummy_cycles - 1)
+            m.submodules.recv_data = recv_data
+            m.submodules.stored_data = stored_data
+            m.d.comb += recv_data.w_data.eq(self.dut.dq.o)
+            with m.If(stored_data.r_rdy & stored_data.r_en):
+                m.d.sync += self.dut.dq.i.eq(stored_data.r_data)
+            with m.FSM() as fsm:
+                with m.State("INIT"):
+                    m.d.sync += data_sig.eq(self.data)
+                    with m.If(self.dut.cs):
+                        # Set w_en 1 clock earlier
+                        with m.If(self.dut.counter == 1):
+                            m.d.comb += recv_data.w_en.eq(1)
+                        with m.Else():
+                            m.d.comb += recv_data.w_en.eq(0)
+                        with m.If(~recv_data.w_rdy & stored_data.w_rdy):
+                            m.next = "PUT-DATA"
+                with m.State("PUT-DATA"): 
+                    with m.If(stored_data_num_sent != 0):
+                        m.d.comb += [
+                            stored_data.w_en.eq(1),
+                            stored_data.w_data.eq(data_sig[-self.dut.spi_width:])
+                        ]
+                        m.d.sync += [
+                            stored_data_num_sent.eq(stored_data_num_sent+1),
+                            data_sig.eq(Cat(Repl(0, self.dut.spi_width), data_sig[:-self.dut.spi_width]))
+                        ]
+                    with m.Else():
+                        m.d.comb += stored_data.w_en.eq(0)
+                    with m.If((dummy_counter != 1) & (self.dut.counter == 0)):
+                        m.d.sync += dummy_counter.eq(dummy_counter - 1)
+                    with m.Elif((dummy_counter == 1) &
+                                (self.dut.counter == self.dut._divisor_val>>1+1)):
+                        m.d.comb += stored_data.r_en.eq(1)
+                        m.next = "RETURN-DATA"
+                with m.State("RETURN-DATA"):
+                    with m.If(self.dut.counter == self.dut._divisor_val>>1+1):
+                        m.d.comb += stored_data.r_en.eq(1)
+                    with m.Else():
+                        m.d.comb += stored_data.r_en.eq(0)
+            return m
+
+    def test_n25q128a_dual(self):
+        self.cmd_dict = {"FAST_READ": 0xbb}
+        self.dut = SPIFlash(protocol="dual", 
+                            addr_width=24,
+                            data_width=8,
+                            cmd_width=8,
+                            cmd_dict=self.cmd_dict)
+        self.flash = (SPIFlashFastReadTestCase.
+                      SuperNaiveDualFlash(dut=self.dut,
+                                          data=0xAA,
+                                          data_size=8))
+        self.simple_test()
+
+    def test_n25q128a_quad(self):
+        self.cmd_dict = {"FAST_READ": 0xeb}
+        self.dut = SPIFlash(protocol="quad", 
+                            addr_width=24,
+                            data_width=8,
+                            cmd_width=8,
+                            cmd_dict=self.cmd_dict)
+        self.flash = (SPIFlashFastReadTestCase.
+                      SuperNaiveDualFlash(dut=self.dut,
+                                          data=0xAA,
+                                          data_size=8))
+        self.simple_test()
+
+    def simple_test(self):
+        m = Module()
+        m.submodules.master = self.dut
+        m.submodules.slave  = self.flash
+        def process():
+            yield self.dut.ack.eq(1)
+            yield self.dut.cmd.eq(self.cmd_dict["FAST_READ"])
+            while (yield self.dut.rdy):
+                yield       # Wait until it enters CMD state
+            yield self.dut.ack.eq(0)
+            while not (yield self.dut.r_rdy):
+                yield       # Wait until it enters RDYWAIT state
+            self.assertEqual((yield self.dut.r_data), self.flash.data)
+        simulation_test(m, process)
